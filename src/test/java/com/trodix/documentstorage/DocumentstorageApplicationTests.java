1d6a99a943ae39872a34ed9cb37e4fa440f58091
Sebastien Vallet <sebastien.vallet89@gmail.com>
2023-01-22 11:43:37 +0100

    initial commit

---
+++ src/test/java/com/trodix/documentstorage/DocumentstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trodix.documentstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentstorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
